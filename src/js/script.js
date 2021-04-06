f591b5ab10e3bc70402dc79ac2e29c3071e95c1d
JozsefVass <joco.udv@gmail.com>
2021-04-06 07:12:13 +0100

    delete

--- src/js/script.js
+++
@@ -1,4 +0,0 @@
-const jqueryUi = require("./modules/jquery-ui.min.js");
-const bootstrap = require("bootstrap");
-const scripts = require("./modules/scripts.js");
-const nav = require("./modules/nav.js");
--- src/js/scrollScript.js
+++
@@ -1,7 +0,0 @@
-import $ from "jquery";
-window.jQuery = $;
-window.$ = $;
-global.jQuery = $;
-
-const scroll = require("./modules/scroll.js");
-const workSlider = require("./modules/work-slider.js");
